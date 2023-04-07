708595a74e5f3ced3e2b9dbdbb2155b02a24293c
AKANKSH CAIMI <113203701+AkankshCaimi@users.noreply.github.com>
2023-04-07 15:58:16 +0530

    Add files via upload

---
+++ Module 1/Sufficient_Balance.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Sufficient_Balance {
+
+    int cost;      // cost of an article
+    int balance;   // balance
+
+
+    function enterCost(int _c) public returns(int){
+        cost = _c;      // input of cost
+        return cost;
+    }
+
+    function enterBalance(int _b) public returns(int){
+        balance = _b;   // input of balance
+        return balance;
+    }
+
+    // using revert()
+    function checkCost() public view {
+        if(cost<0) {
+            revert("Value of cost is Invalid!");
+        } else {
+            revert("Cost is correct");
+        }
+    }
+
+    // uisng require()
+    function checkBalance() public view {
+        require(balance>=0, "Value of balance is Invalid!");
+        revert("Balance is Correct");
+    }
+
+    // using assert()
+    function checkPurchase() public view returns(bool){
+        assert(balance>=0);
+        assert(cost>=0);
+        if(balance>=cost) {
+            return true;        // item can be purcahsed
+        } else {
+            return false;       // item cannot be purschsed
+        }
+    }
+}
