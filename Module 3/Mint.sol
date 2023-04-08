8d67b39b745b4a43fb11e4b373f737acbbaf9573
AKANKSH CAIMI <113203701+AkankshCaimi@users.noreply.github.com>
2023-04-08 15:41:39 +0530

    Add files via upload

---
+++ Module 3/Mint.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// imports from https://www.openzeppelin.com/contracts
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Icarion is ERC20, Ownable {
+    constructor() ERC20("Icarion", "ICA") {
+        _mint(msg.sender, 0 * 10 ** decimals()); // initial total suppy is zero
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
